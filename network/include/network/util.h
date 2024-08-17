8f4b0a01955a0c10c9fa8b1479c7fb27350c1357
BZTWO <3213167962@qq.com>
2024-08-17 17:02:30 +0800

    Util

--- network/include/network/util.h
+++ network/include/network/util.h
@@ -4,12 +4,16 @@
 #include <unistd.h>
 
 namespace network {
-pid_t getPid();
+// 返回当前进程的进程 ID
+pid_t getPid();       
 
-pid_t getThreadId();
+// 返回当前线程的线程 ID
+pid_t getThreadId();   
 
-int64_t getNowMs();
+// 返回当前时间的毫秒数
+int64_t getNowMs();     
+                         
+// 从网络字节序的字节数组中提取一个 32 位整数
+int32_t getInt32FromNetByte(const char *buf);    
 
-int32_t getInt32FromNetByte(const char *buf);
-
-}  // namespace network
+}  /// namespace network
